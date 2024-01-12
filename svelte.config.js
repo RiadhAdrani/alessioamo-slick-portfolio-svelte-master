72e705601d20c66915ab1363566e563ac4ac81a6
Alessio <alessio.amorelli@hotmail.com>
2024-01-12 01:51:38 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte-maste';
+const base = 'E:/Users/aless/Documents/Website/Portfolio/slick-portfolio-svelte-master';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
