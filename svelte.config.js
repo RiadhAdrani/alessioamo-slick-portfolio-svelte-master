0684c3154a90213733951cc9f794508f75cc1c47
Riadh ADRANI <54232641+RiadhAdrani@users.noreply.github.com>
2024-01-12 09:29:05 +0000

    fix: gh actions deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/slick-portfolio-svelte-master';
+const base = '/alessioamo-slick-portfolio-svelte-master';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
